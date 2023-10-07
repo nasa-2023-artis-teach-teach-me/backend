31bf12fc61e5b01dd63552b0f62f19a692360d43
Jean-Shan Chou <jschou@cs.nctu.edu.tw>
2023-10-07 17:01:15 +0800

    feat: update response to GeoJson

--- crud.py
+++ crud.py
@@ -1,10 +1,60 @@
 from sqlalchemy.orm import Session
 from models import Fire
 from typing import List 
-import geopandas as gpd
 import geojson
+import json
 
 def get_fire(db: Session, fire_id: int):
-    return db.query(Fire).filter(Fire.id == fire_id).first()
+    features = []
+    data = db.query(Fire).filter(Fire.id == fire_id).first()
+    point = geojson.Point((data.latitude, data.latitude))
+    properties = {
+        "id": data.id,
+        "country_id": data.country_id,
+        "brightness": data.brightness,
+        "scan": data.scan,
+        "track": data.track,
+        "acq_date": data.acq_date,
+        "acq_time": data.acq_time,
+        "confidence": data.confidence,
+        "bright_t31": data.bright_t31,
+        "frp": data.frp,
+        "daynight": data.daynight
+    }
+    
+    feature = geojson.Feature(geometry=point, properties=properties)
+    features.append(feature)
+    feature_collection = geojson.FeatureCollection(features)
+
+    geojson_string = geojson.dumps(feature_collection, sort_keys=True)
+    geojson_dict = json.loads(geojson_string)
+    return geojson_dict
 
 
+def get_fire_by_date(db: Session, date: str):
+    confidence: int = 70
+    data = list(db.query(Fire).filter(Fire.acq_date == date).filter(Fire.confidence >= confidence).all())
+    features = []
+
+    for data in data:
+        point = geojson.Point((data.latitude, data.latitude,0))
+        properties = {
+            "id": data.id,
+            "country_id": data.country_id,
+            "brightness": data.brightness,
+            "scan": data.scan,
+            "track": data.track,
+            "acq_date": data.acq_date,
+            "acq_time": data.acq_time,
+            "confidence": data.confidence,
+            "bright_t31": data.bright_t31,
+            "frp": data.frp,
+            "daynight": data.daynight
+        }
+        feature = geojson.Feature(geometry=point, properties=properties)
+        features.append(feature)
+        feature_collection = geojson.FeatureCollection(features)
+
+    geojson_string = geojson.dumps(feature_collection, sort_keys=True)
+    geojson_dict = json.loads(geojson_string)
+    return geojson_dict
--- main.py
+++ main.py
@@ -26,9 +26,14 @@ async def count(settings: Annotated[config.Settings, Depends(get_settings)]):
     count = get_transaction_count(settings.MAP_KEY)
     return {"message": "Hello World", "transaction count": count}
 
-@app.get("/api/fire/{fire_id}", response_model=schemas.Fire)
+@app.get("/api/fire/{fire_id}", response_model=None)
 async def fire(fire_id: int,db: Session = Depends(get_db)):
     fire_data = crud.get_fire(db,fire_id)
+    return fire_data
+
+@app.get("/api/fire/date/{date}", response_model=None)
+async def get_fire_by_date(date: str,db: Session = Depends(get_db)):
+    fire_data = crud.get_fire_by_date(db,date)
     return fire_data
 
 def start():
